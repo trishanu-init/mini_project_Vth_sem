7456d9c8944a50342cb78fd97c1e9d50427f30b2
trishanu-init <trishanu.nayak@outlook.com>
2025-05-09 08:03:00 +0530

    initialised docker files

---
+++ Frontend/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+
+import React from 'react';
+import DiseaseAnalysis from './DiseaseAnalysis';
+
+function App() {
+  return (
+    <div>
+      <DiseaseAnalysis />
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/src/DiseaseAnalysis.js
@@ -0,0 +1,12 @@
+import React, { useState ,useEffect} from 'react';
+import Predict from './Predict';
+
+const DiseaseAnalysis = () => {
+  return (
+    <div>
+      <Predict />
+    </div>
+  );
+};
+
+export default DiseaseAnalysis;
